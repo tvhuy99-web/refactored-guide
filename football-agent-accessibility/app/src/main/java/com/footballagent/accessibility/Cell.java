5eec63e09d6d6be58ca9dd8651615aec2075abf0
tvhuy99-web <tvhuy99@gmail.com>
2026-09-11 21:12:54 +0700

    Add row cell model

---
+++ football-agent-accessibility/app/src/main/java/com/footballagent/accessibility/Cell.java
@@ -0,0 +1,11 @@
+package com.footballagent.accessibility;
+
+final class Cell {
+    final String text;
+    final Box box;
+
+    Cell(String text, Box box) {
+        this.text = text;
+        this.box = box;
+    }
+}
